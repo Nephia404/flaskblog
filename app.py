c78f41d0d90424941783a5e0d9ff08729aeba1ca
Nephia404 <82391048+Nephia404@users.noreply.github.com>
2022-05-18 12:57:08 +0900

    article add

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():     #index関数の定義
-    return render_template("index.html")   #index.htmlファイルを読み込む
+    return render_template("index.html")
+
+@app.route("/article1")
+def article1(): #article1関数の定義　
+    return render_template("article1.html")
+
+@app.route("/article2")
+def article2(): #article2関数の定義
+    return render_template("article2.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)   #デバッグモードOn
+    app.run(debug=True)
